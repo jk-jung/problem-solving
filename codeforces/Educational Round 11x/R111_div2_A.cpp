a1f10eaaaef5c846bebc48abff51eba7f387f473
jk.jung <jk.jung@dramancompany.com>
2021-07-17 19:36:47 +0900

    add today's codes

---
+++ codeforces/Educational Round 11x/R111_div2_A.cpp
@@ -0,0 +1,48 @@
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <queue>
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp    make_pair
+#define pb    push_back
+#define F     first
+#define S     second
+#define ab(x) (((x)<0)?-(x):(x))
+
+void solve() {
+    int n;
+    cin >> n;
+    int c = 1;
+
+    n--;
+    for (int i = 1;; i++) {
+        if( n <= 0){
+            cout << i << endl;
+            return;
+        }
+        c += 2;
+        n -= c;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int test_case;
+    cin >> test_case;
+    while (test_case--)
+        solve();
+}
