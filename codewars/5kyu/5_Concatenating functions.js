2c569ccd604cf5db0ef43d23a19d9eb5dc2959ce
jk.jung <jk.jung@dramancompany.com>
2021-08-20 22:09:59 +0900

    add today's codes

---
+++ codewars/5kyu/5_Concatenating functions.js
@@ -0,0 +1,9 @@
+// just a small amount of possible functions to start testing with.
+var addOne = function(e) {return e + 1;};
+var square = function(e) {return e * e;};
+
+// Extend the Function prototype with a method pipe
+
+Function.prototype.pipe = function (f) {
+    return (x) => f(this(x))
+}
