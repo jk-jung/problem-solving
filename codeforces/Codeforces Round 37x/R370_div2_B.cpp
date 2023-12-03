68bf1c0f63641facf9d8f147964de304ca21e319
jk-jung <wjdwnrud3@gmail.com>
2023-12-03 11:19:43 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 37x/R370_div2_B.cpp
@@ -0,0 +1,49 @@
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <cassert>
+#include <set>
+#include <map>
+#include <array>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp    make_pair
+#define pb    push_back
+#define F     first
+#define S     second
+#define ab(x) (((x)<0)?-(x):(x))
+
+void solve() {
+    string s;
+    cin >> s;
+    map<char, int> c;
+    for (char x: s)c[x]++;
+    int r = 0;
+    if(s.size() % 2) r = -1;
+    else {
+        int a = abs(c['U'] - c['D']);
+        int b = abs(c['L'] - c['R']);
+        r = (a + b) / 2;
+    }
+
+    cout << r << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
