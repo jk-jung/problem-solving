7c11c817bf449350c9daf1fb2dff9ea6d1d5038a
jk-jung <wjdwnrud3@gmail.com>
2023-12-21 12:36:25 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 91x/R916_div3_A.cpp
@@ -0,0 +1,48 @@
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <cassert>
+#include <set>
+#include <map>
+#include <array>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp    make_pair
+#define pb    push_back
+#define F     first
+#define S     second
+#define ab(x) (((x)<0)?-(x):(x))
+
+void solve() {
+    map<char, int> ck;
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (char x: s)ck[x]++;
+    int r = 0;
+    for (auto [x, y]: ck)r += (x - 'A' + 1 <= y);
+    cout << r << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int test_case;
+    cin >> test_case;
+    while (test_case--)
+        solve();
+}
---
+++ codeforces/Codeforces Round 91x/R916_div3_B.cpp
@@ -0,0 +1,45 @@
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <cassert>
+#include <set>
+#include <map>
+#include <array>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp    make_pair
+#define pb    push_back
+#define F     first
+#define S     second
+#define ab(x) (((x)<0)?-(x):(x))
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    k++;
+    for (int i = 0; i < n - k; i++)cout << n - i << " ";
+    for (int i = 0; i < k; i++)cout << i + 1 << " ";
+    cout << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int test_case;
+    cin >> test_case;
+    while (test_case--)
+        solve();
+}
