5445bcd68b3f87d5747c5e851bb8b7077316b7fb
jk-jung <wjdwnrud3@gmail.com>
2024-01-23 23:18:13 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 92x/R920_div3_A.cpp
@@ -0,0 +1,44 @@
+#include <cstring>
+#include <cstdio>
+#include <cmath>
+#include <cassert>
+#include <set>
+#include <map>
+#include <array>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp    make_pair
+#define pb    push_back
+#define F     first
+#define S     second
+#define ab(x) (((x)<0)?-(x):(x))
+
+void solve() {
+    vi a(4), b(4);
+    for (int i = 0; i < 4; i++)cin >> a[i] >> b[i];
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    cout << (a[3] - a[0]) * (b[3] - b[0]) << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int test_case;
+    cin >> test_case;
+    while (test_case--)
+        solve();
+}
