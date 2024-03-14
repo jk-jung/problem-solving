b316f4c577f71b8eed5988ebe0fae830f3f72330
jk-jung <wjdwnrud3@gmail.com>
2024-03-14 19:00:00 +0900

    add today's codes

---
+++ codewars/7kyu/7_Find Your Villain Name.py
@@ -0,0 +1,5 @@
+def get_villain_name(x):
+    a = [ "The Evil","The Vile","The Cruel", "The Trashy","The Despicable", "The Embarrassing", "The Disreputable","The Atrocious", "The Twirling",  "The Orange","The Terrifying", "The Awkward"]
+    b = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"]
+
+    return a[x.month - 1] + ' ' + b[x.day % 10]
