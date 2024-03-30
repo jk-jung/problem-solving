33489c359c2e7b48a925fdc5270f09ac94e8a689
Jukyung Jung <wjdwnrud3@gmail.com>
2024-03-30 16:38:00 +0900

    add today's codes

---
+++ codewars/7kyu/7_Maximum Multiple.py
@@ -0,0 +1,11 @@
+def max_multiple(divisor, bound):
+    return bound // divisor * divisor
+
+
+"""
+p1257 L68F39R39R37R90 q
+p9694 F26R63P3641R72L15R98 q
+P4845
+p3641 L82P9694F38 q
+p4845 P1257 R98 F30 P1257 F95 F78 L73 P9694 P9694 R86 R63 q
+"""
