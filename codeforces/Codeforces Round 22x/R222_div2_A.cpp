f5d6db4e312290b34517a363bdaaa0603a3a595c
Jukyung Jung <wjdwnrud3@gmail.com>
2024-05-09 19:00:00 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 22x/R222_div2_A.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+int x, y, z;
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+
+    for (int i = 1; i <= 6; i++) {
+        if (ab(i - a) < ab(i - b)) {
+            x++;
+        } else if (ab(i - a) > ab(i - b)) {
+            z++;
+        } else y++;
+    }
+    cout << x << " " << y << " " << z;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
---
+++ codewars/7kyu/7_Covfefe.py
@@ -0,0 +1,4 @@
+def covfefe(s):
+    if 'coverage' in s: s = s.replace('coverage', 'covfefe')
+    else: s += ' covfefe'
+    return s
