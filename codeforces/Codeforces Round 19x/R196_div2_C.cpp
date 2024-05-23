d20abbdf8ac107b39b6549cb6334b6cae9f2f235
Jukyung Jung <wjdwnrud3@gmail.com>
2024-05-23 16:55:52 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 19x/R196_div2_C.cpp
@@ -0,0 +1,90 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+
+#define MOD 1000000009
+
+class Modular {
+public:
+    ll mod;
+    vector<ll> fac, facInv;
+
+    Modular(int n, ll _mod) {
+        mod = _mod;
+        fac.resize(n + 1), facInv.resize(n + 1);
+
+        fac[0] = 1;
+        for (int i = 1; i <= n; i++) fac[i] = fac[i - 1] * i % mod;
+
+        facInv[n] = inv(fac[n]);
+        for (int i = n; i > 0; i--) facInv[i - 1] = facInv[i] * i % mod;
+    }
+
+    ll pow(ll x, ll y) {
+        ll r = 1;
+        for (x = x % mod; y; y >>= 1, x = (x * x) % mod)
+            if (y & 1) r = (r * x) % mod;
+        return r;
+    }
+
+    ll inv(ll x) { return pow(x, mod - 2); }
+
+    ll nCk(ll n, ll k) {
+        if (n < k || n < 0 || k < 0) return 0;
+        if (k == 0) return 1;
+        return fac[n] * facInv[k] % mod * facInv[n - k] % mod;
+    }
+
+    ll nHk(ll n, ll k) {
+        return nCk(n + k - 1, k);
+    }
+} md(200000, MOD);
+
+
+void solve() {
+    ll n, m, k;
+    cin >> n >> m >> k;
+    ll rest = n - m;
+    ll can = n - rest * k;
+    if (can <= 0) {
+        cout << m << endl;
+        return;
+    }
+
+    ll r = (k - 1) * rest % MOD;
+    ll t = md.pow(2, can / k + 1) * k % MOD - 2 * k + can % k;
+    t %= MOD;
+    if (t < 0)t += MOD;
+    cout << (r + t) % MOD << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
