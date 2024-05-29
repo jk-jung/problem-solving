6b6a6d76bd9fcdfec9f91d5166229cf886b68a8d
Jukyung Jung <wjdwnrud3@gmail.com>
2024-05-29 19:41:10 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 19x/R192_div2_A.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<string> v(n);
+    for (auto &s: v)cin >> s;
+
+    set<int> a, b;
+    for (int i = 0; i < n; i++)for (int j = 0; j < m; j++)if (v[i][j] == 'S')a.insert(i), b.insert(j);
+    cout << n * m - a.size() * b.size();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
