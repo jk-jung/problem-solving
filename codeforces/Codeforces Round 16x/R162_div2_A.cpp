4661c5f9842c6ad2fc459415a11c7ac7880106ea
Jukyung Jung <wjdwnrud3@gmail.com>
2024-07-15 23:35:10 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 16x/R162_div2_A.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+
+void solve() {
+    string a, b;
+    cin >> a >> b;
+    int i = 0;
+    for (char x: b) {
+        if (i + 1 < a.size() && a[i] == x) i++;
+    }
+    cout << i + 1 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
