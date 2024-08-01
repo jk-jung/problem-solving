a15faea4f19bcf99011b63441173b747265b04ed
Jukyung Jung <wjdwnrud3@gmail.com>
2024-08-01 23:17:08 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 15x/R151_div2_A.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve() {
+    int n;
+    cin >> n;
+    if (n <= 2) {
+        cout << -1;
+    } else {
+        cout << n << " " << 2;
+        for (int i = 1; i <= n; i++) {
+            if (i == n || i == 2)continue;
+            cout << " " << i;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
