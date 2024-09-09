7e5fccd7ef3a58fdf7928e04f21a4dc09b8cbae8
Jukyung Jung <wjdwnrud3@gmail.com>
2024-09-09 10:55:26 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 97x/R971_div4_C.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve() {
+    int a, b, k;
+    cin >> a >> b >> k;
+    a = (a + k - 1) / k;
+    b = (b + k - 1) / k;
+    if (a > b)cout << a * 2 - 1 << endl;
+    else cout << b * 2 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int test_case;
+    cin >> test_case;
+    while (test_case--)
+        solve();
+}
