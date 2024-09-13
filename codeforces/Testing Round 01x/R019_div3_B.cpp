0dd0aba7a10241c902196a0d0bee5b3327f7147d
Jukyung Jung <wjdwnrud3@gmail.com>
2024-09-13 09:37:59 +0900

    add today's codes

---
+++ codeforces/Testing Round 01x/R019_div3_B.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout << 6 - a - b << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
