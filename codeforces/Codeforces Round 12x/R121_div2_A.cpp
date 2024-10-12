fa530ebbe794652a1c9e8fd3e0c38904c0e36067
Jukyung Jung <wjdwnrud3@gmail.com>
2024-10-12 23:20:37 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 12x/R121_div2_A.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve() {
+    int n;
+    map<int, int> c;
+    cin >> n;
+    for(int i=1;;i++){
+        int k = i * (i + 1) / 2;
+        if(k > n)break;
+        c[k] = 1;
+    }
+    for(auto[x, y]: c){
+        if(c.count(n - x)){
+            cout <<"YES";
+            return;
+        }
+    }
+    cout <<"NO";
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
---
+++ codewars/7kyu/7_Minimum Ticket Cost.js
@@ -0,0 +1,3 @@
+function findJane(n){
+    return parseInt(n / 2) - 1 + n % 2;
+}
