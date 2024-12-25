5fd318620caf5219b76a0c5c5d951bcc2706e779
Jukyung Jung <wjdwnrud3@gmail.com>
2024-12-25 15:09:30 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 07x/R079_div2_B.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+
+string f(string s) {
+    int r = 0;
+    for(char x:s) r += x - '0';
+    return to_string(r);
+}
+
+void solve() {
+    string s;
+    cin >> s;
+    int r = 0;
+    while(s.size() > 1) {
+        s = f(s);
+        r++;
+    }
+    cout << r << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    solve();
+}
