3560e6633e3c311754e59d7d69f848a936c33c0c
Jukyung Jung <wjdwnrud3@gmail.com>
2025-01-24 20:35:12 +0900

    add today's codes

---
+++ codeforces/Codeforces Round 99x/R998_div3_A.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve() {
+    int a, b, d, e;
+    cin >> a >> b >> d >> e;
+    int r = 0;
+    for (int c = -1000; c <= 1000; c++) {
+        int t = 0;
+        if (a + b == c)t++;
+        if (b + c == d)t++;
+        if (c + d == e)t++;
+        r = max(r, t);
+    }
+    cout << r << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int test_case;
+    cin >> test_case;
+    while (test_case--)
+        solve();
+}
---
+++ codewars/7kyu/7_Ch4113ng3.py
@@ -0,0 +1,2 @@
+def nerdify(t):
+    return t.replace('a', '4').replace('A', '4').replace('e', '3').replace('E', '3').replace('l', '1')
