9b024f6d75cfde9d371a2d8604d069e906391f1c
Jukyung Jung <wjdwnrud3@gmail.com>
2025-07-23 21:25:52 +0900

    add today's codes

---
+++ codewars/8kyu/8_ Switch it Up!.py
@@ -0,0 +1,13 @@
+def switch_it_up(number):
+    return [
+        "Zero",
+        "One",
+        "Two",
+        "Three",
+        "Four",
+        "Five",
+        "Six",
+        "Seven",
+        "Eight",
+        "Nine",
+    ][number]
