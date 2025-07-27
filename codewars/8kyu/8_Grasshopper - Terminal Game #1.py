cc41262fb0fec2ae002617e762c8041c7dfbe269
Jukyung Jung <wjdwnrud3@gmail.com>
2025-07-27 12:07:34 +0900

    add today's codes

---
+++ codewars/8kyu/8_Grasshopper - Terminal Game #1.py
@@ -0,0 +1,8 @@
+class Hero(object):
+    def __init__(self, name="Hero"):
+        self.name = name
+        self.position = "00"
+        self.experience = 0
+        self.position = "00"
+        self.health = 100
+        self.damage = 5
