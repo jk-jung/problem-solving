ed9fef558b837f49a7729c3a7067555423e2276b
Jukyung Jung <wjdwnrud3@gmail.com>
2025-08-21 21:18:59 +0900

    Add today’s code

---
+++ codeforces/Codeforces Round 88x/R887_div2_A.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve() {
+  int n;
+  cin >> n;
+  vi v(n);
+  for (int &x : v)
+    cin >> x;
+
+  int r = 1 << 30;
+  for (int i = 0; i < n; i++) {
+    for (int j = i + 1; j < n; j++) {
+      int d = v[j] - v[i];
+      if (d < 0)
+        r = 0;
+      else
+        r = min(r, d / 2 + 1);
+    }
+  }
+  cout << r << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int test_case;
+  cin >> test_case;
+  while (test_case--)
+    solve();
+}
---
+++ codewars/8kyu/8_MakeUpperCase.py
@@ -0,0 +1,1 @@
+make_upper_case = lambda x: x.upper()
