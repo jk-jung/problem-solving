d08aa2b0f769d5b3dba403aebd62e8f31bb045ca
Jukyung Jung <wjdwnrud3@gmail.com>
2025-08-30 10:57:00 -0700

    Add today’s code

---
+++ codeforces/Codeforces Round 104x copy/R1043_div3_B.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve() {
+  ll n;
+  cin >> n;
+  vector<ll> v;
+  for (ll i = 10; n / (i + 1) > 0; i *= 10) {
+    ll k = i + 1;
+    if (n % k == 0)
+      v.pb(n / k);
+  }
+  sort(v.begin(), v.end());
+  cout << v.size() << endl;
+  for (ll x : v)
+    cout << x << " ";
+  cout << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int test_case;
+  cin >> test_case;
+  while (test_case--)
+    solve();
+}
