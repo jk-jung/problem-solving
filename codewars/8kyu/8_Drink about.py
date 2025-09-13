613dce3cfa00694cb99b70cc30fa843b0ff569b1
Jukyung Jung <wjdwnrud3@gmail.com>
2025-09-13 22:46:59 +0900

    Add today’s code

---
+++ codewars/8kyu/8_Drink about.py
@@ -0,0 +1,8 @@
+def people_with_age_drink(age):
+    if age < 14:
+        return "drink toddy"
+    if age < 18:
+        return "drink coke"
+    if age < 21:
+        return "drink beer"
+    return "drink whisky"
