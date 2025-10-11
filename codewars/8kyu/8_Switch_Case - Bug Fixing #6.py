749d32c2ac2e0f535cc4b5d8fc494b51a1f0bc78
Jukyung Jung <wjdwnrud3@gmail.com>
2025-10-11 07:48:41 -0700

    Add today’s code

---
+++ codewars/8kyu/8_Switch_Case - Bug Fixing #6.py
@@ -0,0 +1,16 @@
+def eval_object(v):
+    match v["operation"]:
+        case "+":
+            return v["a"] + v["b"]
+        case "-":
+            return v["a"] - v["b"]
+        case "/":
+            return v["a"] / v["b"]
+        case "*":
+            return v["a"] * v["b"]
+        case "%":
+            return v["a"] % v["b"]
+        case "**":
+            return v["a"] ** v["b"]
+        case _:
+            return 1
