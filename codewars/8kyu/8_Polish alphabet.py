8c28b3a202b25e71756a9af6afe089e35f84030d
Jukyung Jung <wjdwnrud3@gmail.com>
2025-11-24 23:12:34 +0900

    Add today’s code

---
+++ codewars/8kyu/8_Polish alphabet.py
@@ -0,0 +1,15 @@
+m = {
+    "ą": "a",
+    "ć": "c",
+    "ę": "e",
+    "ł": "l",
+    "ń": "n",
+    "ó": "o",
+    "ś": "s",
+    "ź": "z",
+    "ż": "z",
+}
+
+
+def correct_polish_letters(s):
+    return "".join(m.get(x, x) for x in s)
