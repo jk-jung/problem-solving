ea5f29a69279a37094d31cbbb079840b7d327a6f
Jukyung Jung <wjdwnrud3@gmail.com>
2025-12-10 19:03:26 +0900

    Add today’s code

---
+++ codeforces/Codeforces Round 02x/R026_div2_B.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve() {
+  string s;
+  cin >> s;
+
+  vector<int> v;
+  int r = 0;
+  for (char c : s) {
+    if (c == '(')
+      v.pb(1);
+    else if (!v.empty() && v.back() == 1) {
+      v.pop_back();
+      r += 2;
+    } else
+      v.clear();
+  }
+  cout << r << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  solve();
+}
---
+++ codewars/8kyu/8_Holiday VI - Shark Pontoon.py
@@ -0,0 +1,4 @@
+def shark(a, b, c, d, e):
+    if e:
+        d /= 2
+    return "Alive!" if b / d > a / c else "Shark Bait!"
