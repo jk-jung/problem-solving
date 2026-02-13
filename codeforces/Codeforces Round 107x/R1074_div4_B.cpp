2539a0260889dd12b7051eb0cd1968c01da0fe8c
Jukyung Jung <wjdwnrud3@gmail.com>
2026-02-12 21:24:54 -0600

    Add today’s code

---
+++ codeforces/Codeforces Round 107x/R1074_div4_B.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+void solve() {
+  int n;
+  cin >> n;
+  int y = 0;
+  for (int i = 0, x; i < n; i++) {
+    cin >> x;
+    y = max(y, x);
+  }
+  cout << y * n << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int test_case;
+  cin >> test_case;
+  while (test_case--)
+    solve();
+}
