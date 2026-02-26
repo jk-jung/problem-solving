ef358af73d01728344bf88780ca5038af73cc929
Jukyung Jung <wjdwnrud3@gmail.com>
2026-02-26 22:07:37 +0900

    Add today’s code

---
+++ codewars/8kyu/8_Leonardo Dicaprio and Oscars.py
@@ -0,0 +1,8 @@
+def leo(a):
+    if a == 88:
+        return "Leo finally won the oscar! Leo is happy"
+    if a == 86:
+        return "Not even for Wolf of wallstreet?!"
+    if a > 88:
+        return "Leo got one already!"
+    return "When will you give Leo an Oscar?"
