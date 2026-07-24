f9c1835caba211eda490b1c7c2e5e823310cec6d
Jukyung Jung <wjdwnrud3@gmail.com>
2026-07-23 23:33:22 -0700

    Add today’s code

---
+++ codeforces/Codeforces Round 90x/R904_div2_A.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+
+#define mp make_pair
+#define pb push_back
+#define F first
+#define S second
+#define ab(x) (((x) < 0) ? -(x) : (x))
+
+bool f(int x, int k) {
+  int cnt = 0;
+  while (x) {
+    cnt += x % 10;
+    x /= 10;
+  }
+  return cnt % k == 0;
+}
+
+void solve() {
+  int x, k;
+  cin >> x >> k;
+  while (!f(x, k))
+    x++;
+  cout << x << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int test_case;
+  cin >> test_case;
+  while (test_case--)
+    solve();
+}
---
+++ codewars/8kyu/8_Opposites Attract.py
@@ -0,0 +1,2 @@
+def lovefunc(a, b):
+    return a % 2 != b % 2
