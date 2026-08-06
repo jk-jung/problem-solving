d041ca38f44b320da2ae925c2387b998ac49e163
Jukyung Jung <wjdwnrud3@gmail.com>
2026-08-05 23:21:55 -0700

    Add today’s code

---
+++ codewars/8kyu/8_Get Planet Name By ID.py
@@ -0,0 +1,17 @@
+def get_planet_name(id):
+    if id == 1:
+        return "Mercury"
+    if id == 2:
+        return "Venus"
+    if id == 3:
+        return "Earth"
+    if id == 4:
+        return "Mars"
+    if id == 5:
+        return "Jupiter"
+    if id == 6:
+        return "Saturn"
+    if id == 7:
+        return "Uranus"
+    if id == 8:
+        return "Neptune"
